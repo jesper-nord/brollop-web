96a79b54ae888c6dc5ea59a81c5e2d35b3b2f182
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-01-07 22:01:31 +0100

    responsive layout fixes add pattern background, update us pic

--- src/content/home/index.js
+++ src/content/home/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import './home.css'
-import us from '../../images/us.jpg'
+import us from '../../images/us@0.5.jpeg'
 
 export const Home = () => {
   return (
     <div className='content'>
       <p>Vi gifter oss! Välkomna att fira och dela vår kärlek.</p>
       <p>På den här sidan hittar ni all praktisk information ni behöver. Vi hoppas att träffa er i juni.</p>
-      <img src={us} className='us' alt='Jesper och Ulrika' />
+      <img src={us} className='us' alt='Jesper och Ulrika, 2/5 2019' />
     </div>
   )
 }

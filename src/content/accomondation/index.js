b7b78eb40447e237ea81e839b5de27987f8cb9f0
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-01-11 13:43:18 +0100

    styling, some accomondation text

--- src/content/accomondation/index.js
+++ src/content/accomondation/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export const Accomondation = () => {
   return (
-    <p>Information om boende, bokning... </p>
+    <div>
+      <p>
+        Det finns möjlighet att boka övernattning på slottets hotell, och vi blir självklart väldigt glada 
+        om ni skulle välja att stanna över natten. 
+        På <a href='https://www.hesselbyslott.se/hotell/'>Hesselbys slotts hemsida</a> går det att läsa mer om rummen och boka.
+      </p>
+    </div>
   )
 }

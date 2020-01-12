dd67749e0a436bdc147c0c38d50e38242b554d8d
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-01-12 17:08:19 +0100

    image styling

--- src/content/home/index.js
+++ src/content/home/index.js
@@ -8,7 +8,7 @@ export const Home = () => {
     <div className='content'>
       <p>Vi gifter oss! Välkomna att fira och dela vår kärlek.</p>
       <p>På den här sidan hittar ni all praktisk information ni behöver. Vi hoppas att träffa er i juni.</p>
-      <img src={us} className='us' alt='Jesper och Ulrika, 2/5 2019' />
+      <img src={us} className='us image-border' alt='Jesper och Ulrika, 2/5 2019' />
       <p className='img-text'>New York 2/5 2019, dagen då vi förlovade oss</p>
     </div>
   )
--- src/content/information/index.js
+++ src/content/information/index.js
@@ -40,25 +40,25 @@ export const Information = ({ navigate }) => {
           Vill ni hålla tal så mailar ni så snart som möjligt till <Link href='mailto:toastmasters@ulrikajesper2020.se'>toastmasters@ulrikajesper2020.se</Link>. 
         </p>
         <p>Tal är underbart! Men försök att hålla det kort, gärna under 3 minuter.</p>
-        <p className='presentation'>
+        <div className='presentation'>
           <h2>Daniela</h2>
-          <img className='img-presentation' src={daniela} alt='Daniela' />
+          <img className='img-presentation image-border' src={daniela} alt='Daniela' />
           <p>
             Daniela är Ulrikas barndomsvän och de gick i skolan tillsammans från årskurs 7. 
             De har gjort mycket roligt tillsammans, bland annat rest till ett flertal länder och varit på ridläger tillsammans.
             Det är tack vare Daniela att bröllopet överhuvudtaget blir av, eftersom hon tog med Jesper till en oktoberfest på Ulrikas jobb där de träffades för första gången. 
             Utöver sin roll som toastmadame är Daniela även Ulrikas brudtärna.
           </p>
-        </p>
-        <p className='presentation'>
+        </div>
+        <div className='presentation'>
           <h2>Thomas</h2>
-          <img className='img-presentation' src={thomas} alt='Thomas' />
+          <img className='img-presentation image-border' src={thomas} alt='Thomas' />
           <p>
             Thomas och Jesper har känt varandra i flera år, med ett gemensamt intresse för brädspel, datorer och öl. 
             De har även hunnit med att jobba på två jobb tillsammans i konsultbranchen.
             Utöver sin roll som toastmaster är Thomas även Jespers best man.
           </p>
-        </p>
+        </div>
       </section>
       <section className='section'>
         <h1>Presenter</h1>

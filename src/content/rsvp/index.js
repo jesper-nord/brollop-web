ed16aa3effbf2f44879115afdfafc82403b7eb48
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-02-01 22:58:31 +0100

    update rsvp

--- src/content/rsvp/index.js
+++ src/content/rsvp/index.js
@@ -7,9 +7,8 @@ export const RSVP = () => {
       <section className='section'>
         <h1>O.S.A.</h1>
         <p>O.S.A. senast den x/x 2020 till <Link href='mailto:osa@ulrikajesper2020.se'>osa@ulrikajesper2020.se</Link>.</p>
-      </section>
-      <section className='section'>
-        <h1>Allergier</h1>
+        <br />
+        <h2>Allergier</h2>
         <p>Glöm inte att meddela eventuella allergier i samband med att du O.S.A:r</p>
       </section>
     </div>

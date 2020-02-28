7e5c31e3f171a434beb028bbd0c650483fac69c2
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-02-28 22:11:14 +0100

    lazy load header background image

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './header.css'
+import { useLazyLoad } from '../../hooks/useLazyLoad'
+import banner from '../../images/hesselby-slott.jpg'
 
 const Header = () => {
+  const headerLoaded = useLazyLoad(banner)
+
+  const backgroundStyle = { backgroundImage: headerLoaded ? `url(${headerLoaded})` : '' }
+
   return (
-    <header className='header'>
+    <header className={['header', headerLoaded ? 'loaded' : ''].join(' ')} style={backgroundStyle}>
       <div className='header-title-holder'>
         <span className='header-title'>Ulrika &amp; Jesper</span>
         <span className='header-subtitle'>27 juni 2020</span>

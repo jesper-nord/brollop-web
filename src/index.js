cf77956933179a8f9af3c0afaf776012bb1ba534
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-03-07 18:21:32 +0100

    GA init settings

--- src/index.js
+++ src/index.js
@@ -5,7 +5,12 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-ReactGA.initialize('UA-159992142-1')
+ReactGA.initialize('UA-159992142-1', {
+  debug: window.location.host.includes('localhost'),
+  gaOptions: {
+    siteSpeedSampleRate: 100
+  }
+})
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

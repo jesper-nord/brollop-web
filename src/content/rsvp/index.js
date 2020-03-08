327700565ed49fcab3ab40ec86027e4725280803
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-03-08 11:30:21 +0100

    update rsvp page

--- src/content/rsvp/index.js
+++ src/content/rsvp/index.js
@@ -8,7 +8,9 @@ export const RSVP = () => {
         <h1>O.S.A.</h1>
         <p>O.S.A. senast den 4 maj 2020 till <Link href='mailto:osa@ulrikajesper2020.se'>osa@ulrikajesper2020.se</Link>.</p>
         <p>Meddela gärna i O.S.A. om ni planerar att sova över på slottets hotell.</p>
-        <h2>Allergier</h2>
+      </section>
+      <section className='section'>
+        <h1>Matallergier</h1>
         <p>Glöm inte att meddela eventuella allergier i samband med att du O.S.A:r</p>
       </section>
     </div>

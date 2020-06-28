84a480d1ec5f725507398272532fe82ad8696be6
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-06-28 13:07:58 +0200

    add external icon to external links

--- src/components/link/index.js
+++ src/components/link/index.js
@@ -1,6 +1,14 @@
 import React from 'react'
 import styles from './link.module.scss'
+import externalLinkIcon from '../../images/external-link.png'
 
 export const Link = ({ href, children, external = false }) => {
-  return <a href={href} className={styles.link} target={external ? '_blank' : '_self'} rel='noopener noreferrer'>{children}</a>
+  return (
+    <>
+      <a href={href} className={styles.link} target={external ? '_blank' : '_self'} rel='noopener noreferrer'>
+        {children}
+      </a>
+      {external && <img style={{ opacity: 0.7, width: 14, height: 14, margin: '0 0 -2px 3px' }} src={externalLinkIcon} alt='Extern' />}
+    </>
+  )
 }

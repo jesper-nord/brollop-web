9c17b780aaa06111c6f717ee528bba6f3cae05bd
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-07-10 18:29:24 +0200

    add support for graphcms for a single page

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,20 @@ export const EMAILS = {
   TOASTMASTERS: `toastmasters@${DOMAIN}`,
   RSVP: `osa@${DOMAIN}`
 }
+
+
+export const GRAPHCMS_ENDPOINT = 'https://api-eu-central-1.graphcms.com/v2/ckc7ycwra09i001xr559jgvds/master'
+
+export const getPageContentQuery = id => {
+  return `
+    {
+      pageContent(where: { 
+        id: "${id}" 
+      }) {
+        textContent {
+          html
+        }
+      }
+    }
+  `
+}

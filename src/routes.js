4d9a9017cac1c26ee450062e5436c7206a8b528d
Jesper Lehtinen <jsprlehtinen@gmail.com>
2020-09-03 22:03:15 +0200

    Update pics content page

--- src/routes.js
+++ src/routes.js
@@ -1,33 +1,33 @@
-import { Home, RichTextContent, Toastmasters } from './content';
+import { Home, RichTextContent, Toastmasters } from "./content";
 
 export const routes = {
   home: {
-    title: 'Hem',
-    contentType: Home
+    title: "Hem",
+    contentType: Home,
   },
   information: {
-    title: 'Information',
+    title: "Information",
     contentType: RichTextContent,
-    contentId: 'ckc7ylsmg0iir01545re6b3n4'
+    contentId: "ckc7ylsmg0iir01545re6b3n4",
   },
   toastmasters: {
-    title: 'Toastmasters',
+    title: "Toastmasters",
     contentType: Toastmasters,
-    contentId: 'ckcgg3gio0d4n0153embbxkp7'
+    contentId: "ckcgg3gio0d4n0153embbxkp7",
   },
   pictures: {
-    title: 'Bilder',
+    title: "Bilder",
     contentType: RichTextContent,
-    contentId: 'ckcggto6w043w014871is41p0'
+    contentId: "cken8heuo02b3015456787563",
   },
   accomondation: {
-    title: 'Boende & Hitta hit',
+    title: "Boende & Hitta hit",
     contentType: RichTextContent,
-    contentId: 'ckc7ywymo0ikz0154g1xb362c'
+    contentId: "ckc7ywymo0ikz0154g1xb362c",
   },
   rsvp: {
-    title: 'O.S.A.',
+    title: "O.S.A.",
     contentType: RichTextContent,
-    contentId: 'ckcgfuo5s03to0148x2rbfhd7'
-  }
+    contentId: "ckcgfuo5s03to0148x2rbfhd7",
+  },
 };
